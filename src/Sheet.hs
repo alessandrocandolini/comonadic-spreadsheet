b1776a33a5e8a9551a83e87b2d409e137086c77b
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-09-12 00:01:46 +0100

    importing comonad

--- src/Sheet.hs
+++ src/Sheet.hs
@@ -1,1 +1,19 @@
 module Sheet where
+
+import Data.Function (fix)
+
+fix1 :: (a -> a) -> a
+fix1 f = a where a = f a
+
+class CoFlatMap f where
+  coFlatMap :: (f a -> b) -> f a -> f b
+
+loeb :: (Functor f) => f (f a -> a) -> f a
+loeb ff = fix1 $ \fa -> fmap ($ fa) ff
+
+class (Functor f) => Comonad f where
+  extract :: f a -> a
+  cojoin :: f a -> f (f a)
+
+
+data Stream a = Stream a (Stream a) deriving (Eq, Show)
