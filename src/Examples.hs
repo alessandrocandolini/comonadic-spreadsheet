d40a3b40293cb0a3d2c6e7348baea524fa3b1a83
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-09-12 02:21:03 +0100

    wip

--- src/Examples.hs
+++ src/Examples.hs
@@ -1,1 +1,51 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TypeFamilies #-}
 module Examples where
+import Prelude hiding (head)
+import Control.Comonad
+import GHC.Exts
+
+data MyNonEmpty a = MyNonEmpty a [a] deriving (Eq, Show, Functor)
+
+instance IsList (MyNonEmpty a) where
+  type Item (MyNonEmpty a) = a
+  fromList = from
+  toList = to
+
+from [] = error "cannot accept empty list"
+from (a:as) = MyNonEmpty a as
+
+to (MyNonEmpty a as) = a : as
+
+
+nonEmpty :: [a] -> Maybe (MyNonEmpty a )
+nonEmpty [] = Nothing
+nonEmpty (h:t) = Just (MyNonEmpty h t)
+
+instance Semigroup (MyNonEmpty a) where
+  (MyNonEmpty a1 t1) <> (MyNonEmpty a2 t2) = MyNonEmpty a1 (t1 ++ (a2 : t2))
+
+
+head :: MyNonEmpty a -> a
+head (MyNonEmpty a _ ) = a
+
+tails :: MyNonEmpty a -> MyNonEmpty (MyNonEmpty a)
+tails n@(MyNonEmpty _ t) =  case nonEmpty t of
+   Nothing -> MyNonEmpty n []
+   Just t1 -> MyNonEmpty n [] <> tails t1
+
+instance Comonad MyNonEmpty where
+  extract = head
+  duplicate = tails
+
+
+coFlatMap :: Comonad w => (w a -> b) -> w a -> w b
+coFlatMap f = fmap f . duplicate
+
+newtype WindowSize = WindowSize Int deriving (Eq,Show)
+    deriving (Num,Ord,Read) via Int
+
+sma :: Num a => WindowSize -> MyNonEmpty a -> [Double]
+sma size = const []
+
--- test/ExamplesSpec.hs
+++ test/ExamplesSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedLists #-}
 module ExamplesSpec where
 
 import Examples
@@ -10,6 +11,18 @@ spec = describe "Simple test" $ do
 
      it "example-based unit test" $
         1 `shouldBe` 1
+
+     it "tails of a nonempty list of multiple elements" $
+        tails (MyNonEmpty 1 [2,3]) `shouldBe` (MyNonEmpty (MyNonEmpty 1 [2,3]) [(MyNonEmpty 2 [3]), (MyNonEmpty 3 [])])
+
+     it "tails of a nonempty list of one element" $
+        tails (MyNonEmpty 1 []) `shouldBe` (MyNonEmpty (MyNonEmpty 1 []) [])
+
+     it "tails of a nonempty list of multiple elements (overloaded syntax)" $
+        tails [1,2,3] `shouldBe` [[1,2,3], [2, 3], [3]]
+
+     it "tails of a nonempty list of one element (overloaded syntax)" $
+        tails [1] `shouldBe` [[1]]
 
      prop "property-based unit test" $
         \l -> reverse ( reverse l ) == ( l::[Int])
